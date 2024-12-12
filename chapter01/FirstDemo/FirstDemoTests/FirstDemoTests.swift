e4476cf5a764f88a93f9cf0ba25562060d7f25ff
velmash <hyengchan@gmail.com>
2024-12-12 23:38:54 +0900

    :pencil2: Test Code 메소드 관련 설명 추가

--- chapter01/FirstDemo/FirstDemoTests/FirstDemoTests.swift
+++ chapter01/FirstDemo/FirstDemoTests/FirstDemoTests.swift
@@ -9,15 +9,18 @@ import XCTest
 @testable import FirstDemo
 
 final class FirstDemoTests: XCTestCase {
-
+    
+    // Test 시작 전 호출되는 함수
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    // 모든 Test 완료 후 호출되는 함수
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
+    // 기본적인 Unit Test 단위
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -26,6 +29,7 @@ final class FirstDemoTests: XCTestCase {
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
+    // 10회 정도 돌린 후 시간 측정하는 함수 (measure 안에 있는 코드)
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
